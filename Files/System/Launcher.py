32a8c69909406f6a1f7428b25ff726b5145e40aa
BlackPan8er <149423360+harismastergamer@users.noreply.github.com>
2024-07-01 18:12:36 +0300

    Update Launcher.py

--- Files/System/Launcher.py
+++ Files/System/Launcher.py
@@ -1,7 +1,35 @@
-from time import sleep
+import tkinter as tk
 
-print("This File Is Downloaded From Github!")
-sleep(1)
-print("exiting in 5s...")
-sleep(5)
-quit()
+
+
+
+
+def WantToLeave():
+    WantToLeaveWin = tk.Tk()
+    WantToLeaveWin.geometry("200x150")
+    wanttoleavelabel = tk.Label(WantToLeaveWin, text="Are You Sure You Want To\nLeave?\nThis May Stop An Update\nAnd Destroy The Installation!")
+    wanttoleavelabel.place(x=20, y=20)
+    WantToLeaveWin.grab_set()
+    yesbutton = tk.Button(WantToLeaveWin, text="Yes",command=quit)
+    nobutton = tk.Button(WantToLeaveWin, text="No",command=WantToLeaveWin.destroy)
+    yesbutton.place(x=150, y=120)
+    nobutton.place(x=10, y=120)
+    
+
+
+UpdateWin = tk.Tk()
+UpdateWin.geometry("300x200")
+UpdateWin.title("Loading AHT")
+UpdateWin.resizable(width=False, height=False)
+UpdateWin.protocol("WM_DELETE_WINDOW", WantToLeave)
+
+
+
+
+
+statuslabel = tk.Label(UpdateWin, text="Loading...", font=("Arial", 15))
+
+statuslabel.place(relx=0.5, rely=0.2, anchor='center')
+
+
+UpdateWin.mainloop()
