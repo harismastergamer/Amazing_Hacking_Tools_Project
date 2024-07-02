119afcbbf88eecef59431968c607f4838dede384
BlackPan8er <149423360+harismastergamer@users.noreply.github.com>
2024-07-02 18:39:47 +0300

    Update Updater.py

--- Files/System/Updater.py
+++ Files/System/Updater.py
@@ -1,11 +1,17 @@
 import tkinter as tk
 import os
-global InstallLoc
+import requests
+
+AssetsConfigUrl = "https://raw.githubusercontent.com/harismastergamer/Amazing_Hacking_Tools_Project/main/Files/Assets/AssetConfig.yaml"
 
 
-with open(os.path.dirname(os.path.abspath(__file__)), "r") as InstallLocFile:
+with open(os.path.dirname(os.path.abspath(__file__) + "/ScriptFolderLoc.txt"), "r") as InstallLocFile:
     InstallLoc = InstallLocFile.read()
     InstallLocFile.close()
 
-print(InstallLoc)
+print("InstallLoc: ",InstallLoc)
 
+if not os.path.exists(InstallLoc + "/System/AssetsConfig.yaml"):
+    response = requests.get(AssetsConfigUrl)
+    with open(InstallLoc + "/System/AssetsConfig.yaml", "w") as AssetsConfigFile:
+        AssetsConfigFile.write(response.content)
